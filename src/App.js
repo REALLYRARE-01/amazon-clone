070e00d5ccde1d921b389291e5fb1ed8906cfc3e
REALLYRARE-01 <111707496+REALLYRARE-01@users.noreply.github.com>
2022-10-07 12:34:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,18 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter as Router, Switch, Route }
-from "react-router-dom";
-import Checkout from "./Checkout";
+
 
 function App() {
   return (
     // BEM
-    <Router>
     <div className="app">
-      <Switch>
-        <Route path="/checkout">
-          <Header/>
-          <Checkout />
-        </Route>
-        <Route path="/">
-          <Header />
-          <Home />
-        </Route>
-      </Switch>
-    
+    <Header/>
+    <Home />
+
+ \
     </div>
-    </Router>
+  
   );
 }
 
